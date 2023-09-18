3e9050fe987a45911b5f072fbf112a7a38fb315e
Vladislav Osin <osinvladislav@yandex.ru>
2023-09-19 00:08:33 +0300

    Web fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:window_manager/window_manager.dart';
@@ -9,8 +10,8 @@ import 'pages/main_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
-  if (Platform.isWindows) {
+  if (!kIsWeb && Platform.isWindows) {
+    await windowManager.ensureInitialized();
     WindowManager.instance.setMinimumSize(const Size(1000, 500));
     WindowManager.instance.setTitle('Решение задач линейного программирования');
   }
@@ -27,6 +28,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Решение задач линейного программирования',
       scrollBehavior: ScrollConfiguration.of(context).copyWith(
         dragDevices: {
           PointerDeviceKind.touch,
